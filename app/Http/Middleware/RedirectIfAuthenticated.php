b515187dd244341501ba06bdac385c035a98aa4b
T.M. Syahril Nur A <syahrilnur@mhs.unsyiah.ac.id>
2021-07-22 14:43:00 +0700

    :hammer: update use Illuminate\Support\Facades\DB;

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -17,10 +17,14 @@ class RedirectIfAuthenticated
      * @param  string|null  ...$guards
      * @return mixed
      */
-    public function handle($request, Closure $next, $guard = null)
+    public function handle($request, Closure $next, ...$guards)
     {
-        if (Auth::guard('admin')->check()) {
-            return redirect(route('admindashboard'));
+        $guards = empty($guards) ? [null] : $guards;
+
+        foreach ($guards as $guard) {
+            if (Auth::guard($guard)->check()) {
+                return redirect(RouteServiceProvider::HOME);
+            }
         }
 
         return $next($request);
